aa62e33c2adaa574aa012a0418748270327af447
JimboJuice2000 <gauravgrox@gmail.com>
2024-06-21 11:34:45 +0530

    added custom backgrounds for each weather

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const body = document.querySelector("body");
+const card = document.querySelector(".card");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
@@ -21,21 +23,31 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = data.main.temp.toFixed(1) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    card.style.backdropFilter="blur(15px)";
 
     if (data.weather[0].main=="Clouds") {
         weatherIcon.src = "images/clouds.svg";
+        body.style.backgroundImage="url(images/clouds-bg.jpeg)";
     } 
     else if (data.weather[0].main=="Clear") {
         weatherIcon.src = "images/clear.svg";
+        body.style.backgroundImage="url(images/clear-bg.jpeg)";
     } 
     else if (data.weather[0].main=="Rain") {
         weatherIcon.src = "images/rain.svg";
+        body.style.backgroundImage="url(images/rain-bg.jpeg)";
     }
     else if (data.weather[0].main=="Drizzle") {
         weatherIcon.src = "images/drizzle.svg";
+        body.style.backgroundImage="url(images/drizzle-bg.jpeg)";
     }
     else if (data.weather[0].main=="Mist") {
         weatherIcon.src = "images/mist.svg";
+        body.style.backgroundImage="url(images/mist-bg.jpeg)";
+    }
+    else if (data.weather[0].main=="Haze") {
+        weatherIcon.src = "images/mist.svg";
+        body.style.backgroundImage="url(images/haze-bg.jpeg)";
     }
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
